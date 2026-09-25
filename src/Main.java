c44d4ad4405a2cc8b625a74a177dac5ae8e60dc8
Madeleine Nguyen <madeleine.nguyen@hyneskhater.com>
2026-09-24 21:24:12 -0500

    [SC1] Create initial Java project and main class

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Educational Budgeting RPG!");
+        System.out.println("Day 1");
+        System.out.println("Starting budget: $100.00");
+    }
+}
